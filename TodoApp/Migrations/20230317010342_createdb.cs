d70ff5e50cd39abe848910a18124b659fef7b21d
COLLABERA <COLLABERA@COLLABPH1001339>
2023-03-17 17:12:17 +0800

    Add project files.

---
+++ TodoApp/Migrations/20230317010342_createdb.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApp.Migrations
+{
+    public partial class createdb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DOB",
+                value: new DateTime(2003, 3, 17, 9, 3, 42, 585, DateTimeKind.Local).AddTicks(5770));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DOB",
+                value: new DateTime(2003, 3, 16, 9, 41, 50, 520, DateTimeKind.Local).AddTicks(6596));
+        }
+    }
+}
